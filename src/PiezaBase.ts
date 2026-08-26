7f4f7a136db144e4bf7c462721122474ba0c93de
Ana Julieta Sanchez <anajulietasg@gmail.com>
2026-08-26 17:19:31 -0300

    hacer abstracta a clase piezabase y privada a nombre

--- src/PiezaBase.ts
+++ src/PiezaBase.ts
@@ -1,14 +1,22 @@
 import { IRotator } from "./IRotator";
 
-export class PiezaBase 
-             implements IRotator {
-  public nombre: string = "";  //todas las piezas llevan nombre
+export abstract class PiezaBase 
+       implements IRotator {
+  private _nombre: string = "";
+
+  get nombre(): string {
+    return this._nombre;
+  }
+
+  set nombre(value: string) {
+    this._nombre = value;
+  }
 
   rotateLeft(): void {
-    
+    // cada pieza definirá su rotación
   }
 
   rotateRight(): void {
-    
+    // cada pieza definirá su rotación
   }
 }
